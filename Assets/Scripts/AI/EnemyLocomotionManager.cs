9ea2de2af277ab8b7970636d56b6ecbbf22585bf
Piotr Maszkało <pmaszkal@gmail.com>
2022-09-11 14:07:01 +0200

    feat: enemy detection

---
+++ Assets/Scripts/AI/EnemyLocomotionManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PM
+{
+    public class EnemyLocomotionManager : MonoBehaviour
+    {
+        EnemyManager enemyManager;
+
+        public CharacterStats currentTarget;
+        public LayerMask detectionLayer;
+
+        private void Awake()
+        {
+            enemyManager = GetComponent<EnemyManager>();
+        }
+
+
+        public void HandleDetection()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, enemyManager.detectionRadius, detectionLayer);
+
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                CharacterStats characterStats = colliders[i].GetComponent<CharacterStats>();
+
+                if (characterStats != null)
+                {
+                    //check for team id
+
+                    Vector3 targetDirection = characterStats.transform.position - transform.position;
+                    float viewableAngle = Vector3.Angle(targetDirection, transform.forward);
+
+                    if (viewableAngle > enemyManager.minimumDetectionAngle && viewableAngle < enemyManager.maximumDetectionAngle)
+                    {
+                        currentTarget = characterStats;
+                    }
+                }
+            }
+        }
+    }
+}
